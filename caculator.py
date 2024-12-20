ec8bb935f2949d6eb5b46bf8fb20099264ab9643
Sonu Lodhi <sp95759201@gmail.com>
2024-12-20 22:41:46 +0530

    give the choice to user

--- caculator.py
+++ caculator.py
@@ -17,6 +17,21 @@ check = 'y'
 
 while check == 'y':
 
+    print("Select opration for two oprands:-")
+    print("1. click '+' for adition")
+    print("2. click '-' for adition")
+    print("3. click '*' for adition")
+    print("4. click '/' for adition")
+    print("5. click '//' for adition")
+    print("6. click '%' for adition")
+    print("7. click '**' for adition")
+    choice = input("Please Enter your choise::")
+    num1 = int(input("Enter the first number ::"))
+    num2 = int(input("Enter the Second number ::"))
+
+    if choice == '+':
+        print(f"The addition of ")
+
 
 
     check = input("Enter 'y' for more opration\nEnter 'n' for exit program::")
