75df6724d259694491c8f0863c306b5ed67240f5
Sonu Lodhi <sp95759201@gmail.com>
2024-12-20 22:28:18 +0530

    functions for all oprations

---
+++ caculator.py
@@ -0,0 +1,14 @@
+def addition(a,b):
+    return a+b
+def substraction(a,b):
+    return a-b
+def multiplication(a,b):
+    return a*b
+def division(a,b):
+    return a/b
+def floorValue(a,b):
+    return a//b
+def remainder(a,b):
+    return a%b
+def power(a,b):
+    return a**b
